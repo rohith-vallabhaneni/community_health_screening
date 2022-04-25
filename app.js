901efc4eba8218264b465e2ceae0fa44a14d498d
Rohith Vallabhaneni <rohith.vallabhaneni@gohuddl.com>
2022-04-25 00:47:36 -0500

    Enable CORS for application

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
+app.use(cors({
+    origin: '*'
+}));
 var mongodb = require('./mongodb.js');
 
 global.__root = __dirname + '/';
